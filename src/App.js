4f54724a1f020fd20ab85da4e4b33814af6510ba
SandChik <rohiid.naufal.tif422@polban.ac.id>
2024-09-01 10:18:28 +0700

    Add trailer on movie detail

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
 import './App.css';
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Header from './components/header/Header';
 import Home from './pages/home/home';
 import MovieList from './components/movieList/movieList';
 import Movie from './pages/movieDetail/movie';
+import React from 'react';
 
-function App() {
+const App = () => {
   return (
     <div className="App">
-        <Router>
-          <Header />
-            <Routes>
-                <Route index element={<Home />}></Route>
-                <Route path="movie/:id" element={<Movie />}></Route>
-                <Route path="movies/:type" element={<MovieList />}></Route>
-                <Route path="/*" element={<h1>Error Page</h1>}></Route>
-            </Routes>
-        </Router>
+      <Router>
+        <Header />
+        <Routes>
+          <Route index element={<Home />} />
+          <Route path="movie/:id" element={<Movie />} />
+          <Route path="movies/:type" element={<MovieList />} />
+          <Route path="/*" element={<h1>Error Page</h1>} />
+        </Routes>
+      </Router>
     </div>
   );
-}
+};
 
 export default App;
